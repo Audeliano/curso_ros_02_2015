0e351444164734ef91aa981b430e882fdecdabd4
Audeliano <audeliano@gmail.com>
2015-11-18 20:10:38 -0200

    pacote alterado na aula 3

--- curso_de_ros/src/emissor.cpp
+++ curso_de_ros/src/emissor.cpp
@@ -1,17 +1,35 @@
 #include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "emissor");
-
 	ros::NodeHandle n;
 
+	ros::Publisher conversa_pub = n.advertise<std_msgs::String>("conversa", 1000);
+
 	ros::Rate loop_rate(10);
+	
+	int count = 0;
 	while (ros::ok())
 	{
-		ros::spinOnce();
+		std_msgs::String msg;
 
+		std::stringstream ss;
+		ss<< "Hello World " << count;
+		msg.data = ss.str();
+
+		ROS_INFO("%s", msg.data.c_str());
+
+		conversa_pub.publish(msg);
+
+		ros::spinOnce();
 		loop_rate.sleep();
+		++count;	
 	}
 return 0;
 }
+
+
+
--- curso_de_ros/src/receptor.cpp
+++ curso_de_ros/src/receptor.cpp
@@ -1,4 +1,10 @@
 #include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void conversaCallback(const std_msgs::String::ConstPtr& msg)
+{
+	ROS_INFO("Mensagem recebida: [%s]", msg->data.c_str());
+}
 
 int main(int argc, char **argv)
 {
@@ -6,7 +12,15 @@ int main(int argc, char **argv)
 
 	ros::NodeHandle n;
 
+	ros::Subscriber conversa_sub = n.subscribe("conversa", 1000, conversaCallback);
+
 	ros::spin();
 
 return 0;
 }
+
+
+
+
+
+
